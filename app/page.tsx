9cfe97e31b4e9c9ba8c1deb858d83a4f2c75828e
Dennis <dmarlo96@live.ca>
2025-02-21 20:04:35 -0500

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-const HomePage = () => {
-  return <div>Jobiby Starter project</div>;
-};
-export default HomePage;
+import { Button } from '@/components/ui/button';
+import { Camera } from 'lucide-react';
+
+export default function Home() {
+	return (
+		<div className='h-screen flex items-center justify-center'>
+			<Button>default button</Button>
+			<Button variant='outline' size='icon'>
+				<Camera />
+			</Button>
+		</div>
+	);
+}
