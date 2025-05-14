3e6005f27a4b3286b4adf236a45fde28c0d637bf
Dennis <dmarlo96@live.ca>
2025-05-14 16:08:54 -0400

    Search Form Functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,5 +1,69 @@
+'use client';
+import { Input } from './ui/input';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Button } from './ui/button';
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from '@/components/ui/select';
+import { JobStatus } from '@/utils/types';
+
 function SearchForm() {
-	return <h1 className='text-4xl'>SearchForm</h1>;
+	// set default values
+	const searchParams = useSearchParams();
+	const search = searchParams.get('search') || '';
+	const jobStatus = searchParams.get('jobStatus') || 'All';
+
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		let params = new URLSearchParams();
+
+		// Get search and jobStatus form values
+		const formData = new FormData(e.currentTarget);
+		const search = formData.get('search') as string;
+		const jobStatus = formData.get('jobStatus') as string;
+
+		// Add search and jobStatus parameters to the URL
+		params.set('search', search);
+		params.set('jobStatus', jobStatus);
+
+		router.push(`${pathname}?${params.toString()}`);
+	};
+
+	return (
+		<form
+			className='bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg'
+			onSubmit={handleSubmit}>
+			<Input
+				type='text'
+				placeholder='Search Jobs'
+				name='search'
+				defaultValue={search}
+			/>
+			<Select defaultValue={jobStatus} name='jobStatus'>
+				<SelectTrigger>
+					<SelectValue />
+				</SelectTrigger>
+				<SelectContent>
+					{['All', ...Object.values(JobStatus)].map((jobStatus) => {
+						return (
+							<SelectItem key={jobStatus} value={jobStatus}>
+								{jobStatus}
+							</SelectItem>
+						);
+					})}
+				</SelectContent>
+			</Select>
+			<Button type='submit'>Search</Button>
+		</form>
+	);
 }
 
 export default SearchForm;
