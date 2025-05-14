0d8b37cce893d2fe0624e98d2517b548303cf55a
Dennis <dmarlo96@live.ca>
2025-05-14 15:34:52 -0400

    Get All Jobs Action

--- utils/actions.ts
+++ utils/actions.ts
@@ -36,3 +36,72 @@ export async function createJobAction(
 		return null;
 	}
 }
+
+type GetAllJobsActionTypes = {
+	search?: string;
+	jobStatus?: string;
+	page?: number;
+	limit?: number;
+};
+
+export async function getAllJobsAction({
+	search,
+	jobStatus,
+	page = 1,
+	limit = 10
+}: GetAllJobsActionTypes): Promise<{
+	jobs: JobType[];
+	count: number;
+	page: number;
+	totalPages: number;
+}> {
+	// Grab the current user's id
+	const userId = authenticateAndRedirect();
+
+	try {
+		// Build the whereClause
+		let whereClause: Prisma.JobWhereInput = {
+			clerkId: userId
+		};
+
+		// If search is provided, add it to the whereClause, where search is a substring of position or company
+		if (search) {
+			whereClause = {
+				...whereClause,
+				OR: [
+					{
+						position: {
+							contains: search
+						}
+					},
+					{
+						company: {
+							contains: search
+						}
+					}
+				]
+			};
+		}
+
+		// If jobStatus is provided, add it to the whereClause
+		if (jobStatus && jobStatus !== 'all') {
+			whereClause = {
+				...whereClause,
+				status: jobStatus
+			};
+		}
+
+		// Query the database with the whereClause and orderBy filter to retrieve list of jobs
+		const jobs: JobType[] = await prisma.job.findMany({
+			where: whereClause,
+			orderBy: {
+				createdAt: 'desc'
+			}
+		});
+
+		return { jobs, count: 0, page: 1, totalPages: 0 };
+	} catch (error) {
+		console.error(error);
+		return { jobs: [], count: 0, page: 1, totalPages: 0 };
+	}
+}
