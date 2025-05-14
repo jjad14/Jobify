c84f31ac41740bffe68e682bcf8c36c24e4dbf26
Dennis <dmarlo96@live.ca>
2025-05-14 15:43:07 -0400

    Job Search Setup

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import JobsList from '@/components/JobsList';
+import SearchForm from '@/components/SearchForm';
+import {
+	dehydrate,
+	HydrationBoundary,
+	QueryClient
+} from '@tanstack/react-query';
+import { getAllJobsAction } from '@/utils/actions';
 
-const JobsPage = () => {
-	return <h1 className='text-4xl'>Jobs Page</h1>;
+const JobsPage = async () => {
+	const queryClient = new QueryClient();
+
+	// Prefetch all the jobs
+	await queryClient.prefetchQuery({
+		queryKey: ['jobs', '', 'all', 1],
+		queryFn: () => getAllJobsAction({})
+	});
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<SearchForm />
+			<JobsList />
+		</HydrationBoundary>
+	);
 };
 
 export default JobsPage;
---
+++ components/DeleteJobBtn.tsx
@@ -0,0 +1,5 @@
+function DeleteJobBtn() {
+	return <h1 className='text-4xl'>DeleteJobBtn</h1>;
+}
+
+export default DeleteJobBtn;
